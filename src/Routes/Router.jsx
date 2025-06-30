9971644712a30ced1b52fae844130733d2eddf4c
Al Shahariar Arafat Shawon <shahariararafat7739@gmail.com>
2025-06-30 06:29:28 +0600

    Project Setup Completed

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+import MainLayout from '../Layouts/MainLayout';
+import Home from '../Pages/Home';
+
+const Router = createBrowserRouter(
+    [
+        {
+            path:"/",
+            Component:MainLayout,
+            children:[
+                {
+                    index:true,
+                    Component:Home,
+                }
+            ]
+        }
+    ]
+)
+
+export default Router;
